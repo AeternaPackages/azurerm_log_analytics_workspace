ed271ed544a3daf3649bdaa1ba216e6ae8a06299
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:38:44 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,24 @@
+locals {
+  log_analytics_workspaces = { for k1, v1 in var.log_analytics_workspaces : k1 => { allow_resource_only_permissions = v1.allow_resource_only_permissions, cmk_for_query_forced = v1.cmk_for_query_forced, daily_quota_gb = v1.daily_quota_gb, data_collection_rule_id = v1.data_collection_rule_id, identity = v1.identity, immediate_data_purge_on_30_days_enabled = v1.immediate_data_purge_on_30_days_enabled, internet_ingestion_enabled = v1.internet_ingestion_enabled, internet_query_enabled = v1.internet_query_enabled, local_authentication_disabled = v1.local_authentication_disabled, local_authentication_enabled = v1.local_authentication_enabled, location = v1.location, name = v1.name, reservation_capacity_in_gb_per_day = v1.reservation_capacity_in_gb_per_day, resource_group_name = v1.resource_group_name, retention_in_days = v1.retention_in_days, sku = v1.sku, tags = v1.tags } }
+
+  log_analytics_saved_searches = merge([
+    for k1, v1 in var.log_analytics_workspaces : {
+      for k2, v2 in coalesce(v1.log_analytics_saved_searches, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        log_analytics_workspace_id = module.log_analytics_workspaces.log_analytics_workspaces["${k1}"].id
+      })
+    }
+  ]...)
+}
+
+module "log_analytics_workspaces" {
+  source                   = "git::https://github.com/AeternaModules/azurerm_log_analytics_workspace.git?ref=v4.80.0"
+  log_analytics_workspaces = local.log_analytics_workspaces
+}
+
+module "log_analytics_saved_searches" {
+  source                       = "git::https://github.com/AeternaModules/azurerm_log_analytics_saved_search.git?ref=v4.80.0"
+  log_analytics_saved_searches = local.log_analytics_saved_searches
+  depends_on                   = [module.log_analytics_workspaces]
+}
+
---
+++ variables.tf
@@ -0,0 +1,74 @@
+variable "log_analytics_workspaces" {
+  description = <<EOT
+Map of log_analytics_workspaces, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+Optional:
+    - allow_resource_only_permissions
+    - cmk_for_query_forced
+    - daily_quota_gb
+    - data_collection_rule_id
+    - immediate_data_purge_on_30_days_enabled
+    - internet_ingestion_enabled
+    - internet_query_enabled
+    - local_authentication_disabled
+    - local_authentication_enabled
+    - reservation_capacity_in_gb_per_day
+    - retention_in_days
+    - sku
+    - tags
+    - identity (block)
+Nested log_analytics_saved_searches (azurerm_log_analytics_saved_search):
+    Required:
+        - category
+        - display_name
+        - name
+        - query
+    Optional:
+        - function_alias
+        - function_parameters
+        - tags
+EOT
+
+  type = map(object({
+    location                                = string
+    name                                    = string
+    resource_group_name                     = string
+    allow_resource_only_permissions         = optional(bool) # Default: true
+    cmk_for_query_forced                    = optional(bool)
+    daily_quota_gb                          = optional(number) # Default: -1
+    data_collection_rule_id                 = optional(string)
+    immediate_data_purge_on_30_days_enabled = optional(bool)
+    internet_ingestion_enabled              = optional(bool) # Default: true
+    internet_query_enabled                  = optional(bool) # Default: true
+    local_authentication_disabled           = optional(bool)
+    local_authentication_enabled            = optional(bool)
+    reservation_capacity_in_gb_per_day      = optional(number)
+    retention_in_days                       = optional(number)
+    sku                                     = optional(string)
+    tags                                    = optional(map(string))
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    log_analytics_saved_searches = optional(map(object({
+      category            = string
+      display_name        = string
+      name                = string
+      query               = string
+      function_alias      = optional(string)
+      function_parameters = optional(list(string))
+      tags                = optional(map(string))
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.log_analytics_workspaces) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.log_analytics_workspaces : [for kk in keys(coalesce(v0.log_analytics_saved_searches, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
